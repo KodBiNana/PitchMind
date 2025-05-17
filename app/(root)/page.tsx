ade45d10f955f1aa842a3f3d5694e0bf78be3052
ByteCrest <lbwiththecodes@gmail.com>
2025-05-17 15:17:47 -0700

    project setup, file and folder structuring, Auth pages Ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+    <pre>
+         WELCOME TO PitchMind- An AI-powered Mock Interview practice platform to help you prepare for your next interview and land dream your job. 
+        <br /> Bost your confidence and ace your interviews with an AI-powered Agent that simulates real interview scenerrios. 
+        <br /> think of is as your personal interview coach, available 24/7 to help you practice and improve your skills. 
+
+    </pre>
+    </div>
+  )
+}
+
+export default page
+
---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "sonner";
+
+const MonaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "PitchMind ",
+  description: "- AI-Powered Interview Preparation platform to perfect  and boast your interview skills.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${MonaSans.className}  antialiased pattern`}>
+        {children}
+
+        <Toaster />
+      </body>
+    </html>
+  );
+}
